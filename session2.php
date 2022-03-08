1f6719e164f4eef05c4a856b79c3c53a757aa82c
monahewidy <69719517+monahewidy@users.noreply.github.com>
2022-03-08 03:50:44 +0200

    Add files via upload

---
+++ session2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cha = array("a","z");
+// $next_cha = ++$cha; 
+if (strlen($next_cha) > 1) 
+{
+echo(++$next_cha[0]."</br>".++$next_cha[1]);
+}
+
+
+?>
+
+</body>
+</html>
